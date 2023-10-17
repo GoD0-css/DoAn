ec71a01089128a7f69db63498303aaf81dab0261
GoD0-css <GoD0-css>
2023-10-17 11:53:52 +0700

    first commit

---
+++ Tinder-Clone-React-Native-master/Tinder-Clone-React-Native-master/App.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigation/StackNavigator";
+import { AuthProvider } from "./hooks/useAuth";
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
+    </NavigationContainer>
+  );
+}
